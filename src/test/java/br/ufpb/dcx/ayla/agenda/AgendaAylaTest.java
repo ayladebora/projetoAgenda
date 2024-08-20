77203e09ad2a7522d87afae70a596d0127e376b2
Ayla Dantas Rebouças <ayla@dce.ufpb.br>
2024-08-20 10:02:34 -0300

    Adicionando teste e novos códigos

---
+++ src/test/java/br/ufpb/dcx/ayla/agenda/AgendaAylaTest.java
@@ -0,0 +1,36 @@
+package br.ufpb.dcx.ayla.agenda;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Collection;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AgendaAylaTest {
+
+    @Test
+    public void testaCadastroPesquisaRemocao(){
+        AgendaAyla agenda = new AgendaAyla();
+        Collection<Contato> contatosAchados =
+                agenda.pesquisaAniversariantes(
+                        20,8);
+        assertTrue(contatosAchados.size()==0);
+        agenda.cadastraContato("Maria",20,8);
+        agenda.cadastraContato("José", 20, 8);
+        contatosAchados =
+                agenda.pesquisaAniversariantes(
+                        20,8);
+        assertEquals(2, contatosAchados.size());
+        Contato maria = new Contato("Maria",20,8);
+        assertTrue(contatosAchados.contains(maria));
+        Contato jose = new Contato("José",20,8);
+        assertTrue(contatosAchados.contains(jose));
+        try {
+            boolean removeu = agenda.removeContato("José");
+            assertTrue(removeu);
+        } catch (ContatoInexistenteException e) {
+            fail("Lançou exceção quando não deveria");
+        }
+
+    }
+}
